ebaca25cf0da358e8ed0f338ef3b0b70b63ab8b1
devmatheus <devmatheusp97@gmail.com>
2024-09-20 10:29:51 -0300

    initial commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from '../../styles/styles.module.scss'
+import Logo from '../../../../src/assets/logo.svg'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Image src={Logo} alt='Logo Legaplan' />
+      <h1 className={styles.welcome}>Bem-vindo de volta, Marcus</h1>
+      <p className={styles.date}>Segunda, 1 de dezembro de 2025</p>
+    </header>
+  )
+}
